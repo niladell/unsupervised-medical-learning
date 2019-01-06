0ef8c6c0b65cbc9c115ed39253ad3fbdb0305359
pjaweh <patrick.hlr@gmail.com>
2019-01-06 18:14:32 +0100

    updated dcm-tf script

--- src/util/convert_dcm_to_tfRecord.py
+++ src/util/convert_dcm_to_tfRecord.py
@@ -3,6 +3,8 @@ import numpy as np
 import skimage.io as io
 import tensorflow as tf
 import pydicom
+import matplotlib.pyplot as plt
+import re
 
 
 def _bytes_feature(value):
@@ -11,6 +13,9 @@ def _bytes_feature(value):
 def _int64_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
+def _float_feature(value):
+    return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
 tfrecords_outfile = 'test.tfrecords'
 
 writer = tf.python_io.TFRecordWriter(tfrecords_outfile)
@@ -18,16 +23,29 @@ writer = tf.python_io.TFRecordWriter(tfrecords_outfile)
 filename = 'ct_test.dcm'
 ds = pydicom.dcmread(filename)
 img_raw = ds.PixelData
+print(ds)
 
 height = ds.pixel_array.shape[0]
 width = ds.pixel_array.shape[1]
+location = ds.get('SliceLocation', "(missing)")
+identity = ds.get('PatientID')
+identity_nr = re.findall(r'\d+$', identity)
     
 example = tf.train.Example(features=tf.train.Features(feature={
     'height': _int64_feature(height),
     'width': _int64_feature(width),
-    'image_raw': _bytes_feature(img_raw)}))
+    'image_raw': _bytes_feature(img_raw),
+    'identity_nr' : _int64_feature(int(identity_nr[0])),
+    'slice_location' : _float_feature(location)}))
 
-print(example)    
+
+#print(example)    
 writer.write(example.SerializeToString())
 
 writer.close()
+
+print([ds.pixel_array.min(),ds.pixel_array.max()])
+
+
+#plt.imshow(ds.pixel_array, cmap=plt.cm.bone)
+#plt.show()

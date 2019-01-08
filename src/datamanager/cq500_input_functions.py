384bc0a2d1545e0e36bd498fb90757ddeb1ce9ca
Ines Pereira <pereira.inez@gmail.com>
2019-01-08 16:56:52 +0100

    Second commit.

--- src/datamanager/cq500_input_functions.py
+++ src/datamanager/cq500_input_functions.py
@@ -16,6 +16,73 @@ WIDTH = 512
 CHANNELS = 3
 
 
+###############################################
+# CONVERSION TO TF RECORD WITH PATRICK'S CODE #
+###############################################
+
+# from PIL import Image
+import numpy as np
+# import skimage.io as io
+import tensorflow as tf
+import pydicom
+import matplotlib.pyplot as plt
+import re
+
+path = '/Users/ines/Desktop/dcm_input_functions'
+os.chdir(path)
+
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+def _int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+def _float_feature(value):
+    return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
+for file in os.listdir(path):
+    pattern = re.compile(r'.dcm$')
+    m = re.search(pattern, file)
+    if m is not None:
+    ds = pydicom.dcmread(file)
+    img_raw = ds.PixelData
+    print(ds)
+
+    height = ds.pixel_array.shape[0]
+    width = ds.pixel_array.shape[1]
+    location = ds.get('SliceLocation', "(missing)")
+    identity = ds.get('PatientID')
+    identity_nr = re.findall(r'\d+$', identity)
+
+    example = tf.train.Example(features=tf.train.Features(feature={
+        'height': _int64_feature(height),
+        'width': _int64_feature(width),
+        'image_raw': _bytes_feature(img_raw),
+        'identity_nr': _int64_feature(int(identity_nr[0])),
+        'slice_location': _float_feature(location)}))
+
+    tfrecords_outfile = file+'.tfrecords'
+    writer = tf.python_io.TFRecordWriter(tfrecords_outfile)
+
+    # print(example)
+    writer.write(example.SerializeToString())
+
+    writer.close()
+
+
+# print([ds.pixel_array.min(),ds.pixel_array.max()])
+
+
+# plt.imshow(ds.pixel_array, cmap=plt.cm.bone)
+# plt.show()
+
+
+###############################################
+# DEFINING THE INPUT FUNCTIONS FOR CQ500      #
+###############################################
+
 def input_fn(params):
     """Read CIFAR input data from a TFRecord dataset.
 
@@ -98,3 +165,5 @@ def generate_input_fn(mode='TRAIN'):
         return noise_input_fn
     else:
         raise ValueError('Incorrect mode provided')
+
+

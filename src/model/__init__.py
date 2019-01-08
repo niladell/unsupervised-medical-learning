17ea35758985769b43edd0d178c1883f286d22ed
Nil Adell <nil.adell01@gmail.com>
2019-01-08 22:41:41 +0100

    Remove unused modules

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,11 +1,8 @@
 """Precise model definition """
 
-# from .CIFAR_example_model import ExampleModel
-from .CIFAR_example_model import ExampleModel
 from .basic_model import BasicModel
 from .resBlocks_model import ResModel
 __all__ = (
-    'ExampleModel',
     'BasicModel',
     'ResModel'
 )

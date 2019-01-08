675d12987b7d8e2ab5522a6893f3b7a2980f62e0
Ines Pereira <pereira.inez@gmail.com>
2019-01-08 18:12:42 +0100

    PCA plotting in 3D working and yielding interesting results. 3D plotting with plotly started --> Goal: figure out what the PC represent.

--- src/util/dcm_manipulation.py
+++ src/util/dcm_manipulation.py
@@ -18,81 +18,20 @@ import matplotlib.pyplot as plt
 import re
 # import gdcm
 from tqdm import tqdm
-
-
-# tf.enable_eager_execution()
-
-
-# path='/Users/ines/Desktop/subjects/subject108/Unknown Study/CT 0.625mm'
-# os.chdir(path)
-
-# filename = 'CT000000.dcm'
-#
-# ds = pydicom.dcmread(filename) # direct conversion of ds to tensor does not work!
-
-######################################
-# PLAYING AROUND WITH PATRICK'S CODE #
-######################################
+import random
+import plotly.plotly as py
+import plotly.graph_objs as go
 
 
 path2 = '/Users/ines/Desktop/subjects copy'
 os.chdir(path2)
-
-def _bytes_feature(value):
-    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
-
-
-def _int64_feature(value):
-    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
-
-
-def _float_feature(value):
-    return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
-
-
-tfrecords_outfile = 'test.tfrecords'
-
-
-# filename = 'CT000000.dcm'
-# ds = pydicom.dcmread(filename)
-# print(ds)
-#
-# pixel_data = []
-# for file in os.listdir(path2):
-#     pattern = re.compile(r'.dcm$')
-#     m = re.search(pattern, file)
-#     if m is not None:
-#         # print(file)
-#         # writer = tf.python_io.TFRecordWriter(tfrecords_outfile)
-#         ds = pydicom.dcmread(file)
-#         img_raw = ds.PixelData
-        # height = ds.pixel_array.shape[0]
-        # width = ds.pixel_array.shape[1]
-        # location = ds.get('SliceLocation', "(missing)")
-        # identity = ds.get('PatientID')
-        # identity_nr = re.findall(r'\d+$', identity)
-
-        # example = tf.train.Example(features=tf.train.Features(feature={
-        #     'height': _int64_feature(height),
-        #     'width': _int64_feature(width),
-        #     'image_raw': _bytes_feature(img_raw),
-        #     'identity_nr': _int64_feature(int(identity_nr[0])),
-        #     'slice_location': _float_feature(location)}))
-
-        # save pixel information to list
-        # pixel_data.append(img_raw)
-
-        # # print(example)
-        # writer.write(example.SerializeToString())
-        #
-        # writer.close()
 
 ##################
 # TRYING OUT PCA #
 ##################
 
 
-
+# Loop to create an list with the pixel data per slice
 x = []
 id = []
 for file in tqdm(os.listdir(path2)):
@@ -105,49 +44,31 @@ for file in tqdm(os.listdir(path2)):
         x.append(dcm.pixel_array)
         id.append(identity)
 
-# tft.pca(x, output_dim=4, dtype=tf.float64)
-
-tensor = tf.convert_to_tensor(example) # this also does not work
-
-tensor = tf.convert_to_tensor(img_raw) # works!
-
-
-list_tensors = []
-for data in pixel_data:
-    tensor = tf.convert_to_tensor(data) # works?
-    list_tensors.append(tensor)
-
-## With Tensorflow --> did not work
-# Perform SVD
-singular_values, u, _ = tf.svd(list_tensors)
-# Create sigma matrix
-sigma = tf.diag(singular_values)
-
-## With sklearn
-# Preprocessing:
 x_array = np.asarray(x) # convert to np.array for ease of manipulation
 id_array = np.asarray(id)
+
+## With Tensorflow --> did not work
+# # Perform SVD
+# singular_values, u, _ = tf.svd(list_tensors)
+# # Create sigma matrix
+# sigma = tf.diag(singular_values)
+
+## With sklearn
+
+# If available, upload data (issue with gdcm and MacOs)
+x_array = np.load('pca_experiments_dcms_pix.npy')
+id_array = np.load('pca_experiments_dcms_id.npy')
+
+# Preprocessing:
+
 
 x_reshaped = x_array.reshape(x_array.shape[0], 512*512) # Reshape so you can run sk-learn pca
 # This particular reshaping is inspired by:
 # https://stackoverflow.com/questions/48003185/sklearn-dimensionality-issues-found-array-with-dim-3-estimator-expected-2
 
-#Small experiment
-id_array = np.array([2,3,4,5])
-id_reshaped = id_array.reshape((id_array.shape[0],1,1))
-id_repeat = np.repeat(id_reshaped, 5, axis=1)
-id_repeat = np.repeat(id_repeat, 5, axis=2)
-id_reshaped = id_repeat.reshape(id_repeat.shape[0], 5*5)
-
-id_reshaped = id_array.reshape((id_array.shape[0],1,1))
-id_repeat = np.repeat(id_reshaped, 512, axis=1)
-id_repeat = np.repeat(id_repeat, 512, axis=2)
-id_reshaped = id_repeat.reshape(x_array.shape[0], 512*512)
-
-
-# Normalization
-sc = StandardScaler()
-x_normalized = sc.fit_transform(x_reshaped)
+# # Normalization (not used, for now)
+# sc = StandardScaler()
+# x_normalized = sc.fit_transform(x_reshaped)
 
 # Performing PCA
 pca = PCA(n_components=3)
@@ -159,24 +80,91 @@ principalDf = pd.DataFrame(data = x_train, columns = ['principal component 1', '
 
 # Visualization (commented out code leads to 3D visualization)
 # Tutorial on 3D visualization in matplotlib: https://matplotlib.org/mpl_toolkits/mplot3d/tutorial.html
+
+# number_of_colors = len(id_array)
+#
+# colors = ["#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
+#              for i in range(number_of_colors)]
+# # print(colors)
+
+# Function (courtesy of https://www.quora.com/How-do-I-generate-n-visually-distinct-RGB-colours-in-Python)
+def define_colors(n):
+  ret = []
+  r = int(random.random() * 256)
+  g = int(random.random() * 256)
+  b = int(random.random() * 256)
+  step = 256 / n
+  for i in range(n):
+    r += step
+    g += step
+    b += step
+    r = int(r) % 256
+    g = int(g) % 256
+    b = int(b) % 256
+    ret.append((r,g,b))
+  return ret
+
+
+colors = define_colors(len(set(id_array)))
+# colors = np.array(colors)
+# colors = np.reshape(colors, 14*3)
+
+
+# Plotting
 fig = plt.figure(figsize = (8,8))
 # ax = fig.add_subplot(1,1,1)
-# ax = plt.axes(projection='3d')
-ax = fig.add_subplot(111) #, projection='3d')
+ax = plt.axes(projection='3d')
+ax = fig.add_subplot(111, projection='3d')
 ax.set_xlabel('Principal Component 1', fontsize = 15)
 ax.set_ylabel('Principal Component 2', fontsize = 15)
-# ax.set_zlabel('Principal Component 3', fontsize = 15)
+ax.set_zlabel('Principal Component 3', fontsize = 15)
 ax.set_title('3 component PCA', fontsize = 20)
-set_ids = set(id)
-colors = ['blue', 'black']
+set_ids = set(id_array)
 for iden, color in zip(set_ids, colors):
     idx = id_array == iden
     ax.scatter(principalDf.loc[idx, 'principal component 1'],
                principalDf.loc[idx, 'principal component 2'],
-               color=color
-               )#, principalDf.loc[:, 'principal component 3'])
+               principalDf.loc[idx, 'principal component 3'],
+               cmap=color,
+               )
+ax.legend(set_ids)
 ax.grid()
 plt.show()
+
+# Interactive plotting:
+# https://plot.ly/python/line-and-scatter/
+
+x, y, z = np.random.multivariate_normal(np.array([0,0,0]), np.eye(3), 200).transpose()
+for iden, color in zip(set_ids, colors):
+    idx = id_array == iden
+    trace1 = go.Scatter3d(
+        x=principalDf.loc[idx, 'principal component 1'],
+        y=principalDf.loc[idx, 'principal component 2'],
+        z=principalDf.loc[idx, 'principal component 3'],
+        mode='markers',
+        marker=dict(
+            size=12,
+            line=dict(
+                color=color,
+                width=0.5
+            ),
+            opacity=0.8
+        )
+    )
+
+    data = [trace1]
+    layout = go.Layout(
+        margin=dict(
+            l=0,
+            r=0,
+            b=0,
+            t=0
+        )
+    )
+fig = go.Figure(data=data, layout=layout)
+py.iplot(fig, filename='simple-3d-scatter')
+
+# https://bokeh.pydata.org/en/latest/docs/gallery/iris_splom.html
 
 # Trying to reconstruct the image
 approximation = pca.inverse_transform(x_train)

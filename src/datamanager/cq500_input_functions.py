335a3d898d45f75c836f8ff457c948bde8730d15
Ines Pereira <pereira.inez@gmail.com>
2019-01-10 17:18:59 +0100

    cq500 input function working. Up for verifcation.

--- src/datamanager/cq500_input_functions.py
+++ src/datamanager/cq500_input_functions.py
@@ -31,6 +31,8 @@ def input_fn(params):
 
     def parser(serialized_example):
         """Parses a single tf.Example into image and label tensors."""
+
+        # Parse the serialized data so we get a dict with our data.
         features = tf.parse_single_example(
             serialized_example,
             features={
@@ -38,13 +40,20 @@ def input_fn(params):
                 "width": tf.FixedLenFeature([], tf.int64),
                 "image_raw": tf.FixedLenFeature([], tf.string),
             })
-        image = tf.decode_raw(features["image"], tf.uint8)
+
+        # Decode the raw bytes so it becomes a tensor with type.
+        image = tf.decode_raw(features["image_raw"], tf.uint8)
+
+        # Hard-code the shape
         image.set_shape([CHANNELS * HEIGHT * WIDTH])
 
+        # The type is now uint8 but we need it to be float.
         image = tf.cast(
                     tf.reshape(image, [HEIGHT, WIDTH, CHANNELS]),
                 tf.float32) * (2. / 255) - 1
+
         random_noise = tf.random_normal([noise_dim])
+
         features = {
             'real_images': image,
             'random_noise': random_noise}
@@ -55,7 +64,7 @@ def input_fn(params):
     image_files = [os.path.join(data_dir, 'train.tfrecords')]
     tf.logging.info(image_files)
     dataset = tf.data.TFRecordDataset([image_files])
-    dataset = dataset.map(parser, num_parallel_calls=batch_size) # find a function to be able to use it with many files.
+    dataset = dataset.map(parser, num_parallel_calls=batch_size)
     dataset = dataset.prefetch(4 * batch_size).cache().repeat()
     if USE_ALTERNATIVE:
         dataset = dataset.apply(
@@ -68,6 +77,28 @@ def input_fn(params):
     features, labels = dataset.make_one_shot_iterator().get_next()
     tf.logging.debug('Input_fn: Features %s, Labels %s', features, labels)
     return features, labels
+
+    # Nil's original code, for comparison:
+    # image_files = [os.path.join(data_dir, 'train.tfrecords')]
+    # tf.logging.info(image_files)
+    # dataset = tf.data.TFRecordDataset([image_files])
+    # dataset = dataset.map(parser, num_parallel_calls=batch_size) # find a function to be able to use it with many files.
+    # # https://www.tensorflow.org/guide/performance/datasets: If your data can fit into memory, use the cache
+    # # transformation to cache it in memory during the first epoch, so that subsequent epochs can avoid the
+    # # overhead associated with reading, parsing, and transforming it.
+    # # dataset = dataset.prefetch(4 * batch_size).cache().repeat()
+    # dataset = dataset.prefetch(4 * batch_size).repeat()
+    # if USE_ALTERNATIVE:
+    #     dataset = dataset.apply(
+    #         tf.contrib.data.batch_and_drop_remainder(batch_size))
+    #     tf.logging.warning('Old version: Used \
+    #        tf.contrib.data.batch_and_drop_remainder instead of regular batch')
+    # else:
+    #     dataset = dataset.batch(batch_size, drop_remainder=True)
+    # # Not sure why we use one_shot and not initializable_iterator
+    # features, labels = dataset.make_one_shot_iterator().get_next()
+    # tf.logging.debug('Input_fn: Features %s, Labels %s', features, labels)
+    # return features, labels
 
 
 def noise_input_fn(params):
@@ -104,14 +135,17 @@ def generate_input_fn(mode='TRAIN'):
     else:
         raise ValueError('Incorrect mode provided')
 
-filename = 'CT_head_trauma_unzipped_CQ500CT0 CQ500CT0_Unknown Study_CT PLAIN THIN_CT000000.dcm.tfrecords'
-reader = tf.TFRecordReader()
-reader = tf.data.TFRecordDataset()
-reader.read(filename)
+if __name__ == '__main__':
 
-x = tf.constant("This is string")
-x = tf.decode_raw(x, tf.uint8)
-y = x[:4]
-sess = tf.InteractiveSession()
-print(y.eval())
+    data_dir = './'
+    params = {
+        'batch_size':100,
+        'data_dir': './',
+        'noise_dim': 10
+    }
+
+    features, labels = input_fn(params)
+    print(features)
+    print(labels)
+
 

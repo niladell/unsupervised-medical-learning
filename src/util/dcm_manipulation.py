62610c2bd5b4a2854702ea64dd07287a41d1558d
Ines Pereira <pereira.inez@gmail.com>
2019-01-13 16:47:44 +0100

    Optimization of code.

--- src/util/dcm_manipulation.py
+++ src/util/dcm_manipulation.py
@@ -21,62 +21,49 @@ import plotly
 import plotly.plotly as py
 import plotly.graph_objs as go
 plotly.tools.set_credentials_file(username='InesPereira', api_key='R4GwRqWixPIU0ICrXHeO')
+import pickle
+import time
 
 
-path2 = '/Users/ines/Desktop/subjects copy'
-os.chdir(path2)
 
-##################
-# TRYING OUT PCA #
-##################
+def generate_image_and_id_arrays(path):
+    # Loop to create an list with the pixel data per slice
+    x = []
+    id = []
+    for file in tqdm(os.listdir(path)):
+        pattern = re.compile(r'.dcm$')
+        m = re.search(pattern, file)
+        if m is not None:
+            # print(file)
+            dcm = pydicom.dcmread(file)
+            identity = dcm.get('PatientID')
+            x.append(dcm.pixel_array)
+            id.append(identity)
 
+    x_array = np.asarray(x) # convert to np.array for ease of manipulation
+    id_array = np.asarray(id)
 
-# Loop to create an list with the pixel data per slice
-x = []
-id = []
-for file in tqdm(os.listdir(path2)):
-    pattern = re.compile(r'.dcm$')
-    m = re.search(pattern, file)
-    if m is not None:
-        # print(file)
-        dcm = pydicom.dcmread(file)
-        identity = dcm.get('PatientID')
-        x.append(dcm.pixel_array)
-        id.append(identity)
+    return x_array, id_array
 
-x_array = np.asarray(x) # convert to np.array for ease of manipulation
-id_array = np.asarray(id)
+def PCA_function(x_array, pca):
+    print('Starting PCA on the given data...')
+    # Preprocessing:
+    x_reshaped = x_array.reshape(x_array.shape[0], 512*512) # Reshape so you can run sk-learn pca
+    # This particular reshaping is inspired by:
+    # https://stackoverflow.com/questions/48003185/sklearn-dimensionality-issues-found-array-with-dim-3-estimator-expected-2
 
-## With Tensorflow --> did not work
-# # Perform SVD
-# singular_values, u, _ = tf.svd(list_tensors)
-# # Create sigma matrix
-# sigma = tf.diag(singular_values)
+    # Normalization (recommended everywhere)
+    sc = StandardScaler()
+    x_reshaped = sc.fit_transform(x_reshaped)
+    print('Normalization of the data done!')
 
-## With sklearn
+    # Check out how many components are necessary to explain the above defined variance:
+    # pca.n_components_
 
-# If available, upload data (issue with gdcm and MacOs)
-x_array = np.load('pca_experiments_dcms_pix.npy')
-id_array = np.load('pca_experiments_dcms_id.npy')
+    x_train = pca.fit_transform(x_reshaped)
+    print('Amazing! Your PCA has been completed!')
 
-# Preprocessing:
-
-
-x_reshaped = x_array.reshape(x_array.shape[0], 512*512) # Reshape so you can run sk-learn pca
-# This particular reshaping is inspired by:
-# https://stackoverflow.com/questions/48003185/sklearn-dimensionality-issues-found-array-with-dim-3-estimator-expected-2
-
-# # Normalization (not used, for now)
-# sc = StandardScaler()
-# x_normalized = sc.fit_transform(x_reshaped)
-
-# Performing PCA
-pca = PCA(n_components=3)
-# pca.fit(pixel_stuff) # has to have 2 or less dimensions
-
-x_train = pca.fit_transform(x_reshaped)
-
-principalDf = pd.DataFrame(data = x_train, columns = ['principal component 1', 'principal component 2', 'principal component 3'])
+    return x_train
 
 # Visualization (commented out code leads to 3D visualization)
 # Tutorial on 3D visualization in matplotlib: https://matplotlib.org/mpl_toolkits/mplot3d/tutorial.html
@@ -104,66 +91,145 @@ def define_colors(n):
     ret.append((r,g,b))
   return ret
 
-# Defining colors automatically:
-colors = define_colors(len(set(id_array)))
 
-# Plotting
-fig = plt.figure(figsize = (8,8))
-# ax = fig.add_subplot(1,1,1)
-ax = plt.axes(projection='3d')
-ax = fig.add_subplot(111, projection='3d')
-ax.set_xlabel('Principal Component 1', fontsize = 15)
-ax.set_ylabel('Principal Component 2', fontsize = 15)
-ax.set_zlabel('Principal Component 3', fontsize = 15)
-ax.set_title('3 component PCA', fontsize = 20)
-set_ids = set(id_array)
-for iden, color in zip(set_ids, colors):
-    idx = id_array == iden
-    ax.scatter(principalDf.loc[idx, 'principal component 1'],
-               principalDf.loc[idx, 'principal component 2'],
-               principalDf.loc[idx, 'principal component 3'],
-               cmap=color,
-               )
-ax.legend(set_ids)
-ax.grid()
-plt.show()
+def static_plotting(principalDf, id_array):
+    # Defining colors automatically:
+    colors = define_colors(len(set(id_array)))
 
-# Interactive plotting with plotly:
-data_full =[]
-for iden, color in zip(set_ids, colors):
-    idx = id_array == iden
-    trace1 = go.Scatter3d(
-        x=principalDf.loc[idx, 'principal component 1'],
-        y=principalDf.loc[idx, 'principal component 2'],
-        z=principalDf.loc[idx, 'principal component 3'],
-        name=iden,
-        mode='markers',
-        marker=dict(
-            size=12,
-            line=dict(
-                color=color,
-                width=0.5
-            ),
-            opacity=0.8
+    # Plotting
+    fig = plt.figure(figsize = (8,8))
+    # ax = fig.add_subplot(1,1,1)
+    ax = plt.axes(projection='3d')
+    ax = fig.add_subplot(111, projection='3d')
+    ax.set_xlabel('Principal Component 1', fontsize = 15)
+    ax.set_ylabel('Principal Component 2', fontsize = 15)
+    ax.set_zlabel('Principal Component 3', fontsize = 15)
+    ax.set_title('3 component PCA', fontsize = 20)
+    set_ids = set(id_array)
+    for iden, color in zip(set_ids, colors):
+        idx = id_array == iden
+        ax.scatter(principalDf.loc[idx, 'principal component 1'],
+                   principalDf.loc[idx, 'principal component 2'],
+                   principalDf.loc[idx, 'principal component 3'],
+                   cmap=color,
+                   )
+    ax.legend(set_ids)
+    ax.grid()
+    plt.show()
+
+
+def interactive_plotting(principalDf, id_array):
+    # Interactive plotting with plotly:
+    colors = define_colors(len(set(id_array)))
+    set_ids = set(id_array)
+    data_full =[]
+    for iden, color in zip(set_ids, colors):
+        idx = id_array == iden
+        trace1 = go.Scatter3d(
+            x=principalDf.loc[idx, 'principal component 1'],
+            y=principalDf.loc[idx, 'principal component 2'],
+            z=principalDf.loc[idx, 'principal component 3'],
+            name=iden,
+            mode='markers',
+            marker=dict(
+                size=12,
+                line=dict(
+                    color=color,
+                    width=0.5
+                ),
+                opacity=0.8
+            )
+        )
+        data_full.append(trace1)
+
+    data = data_full
+    layout = go.Layout(
+        margin=dict(
+            l=0,
+            r=0,
+            b=0,
+            t=0
         )
     )
-    data_full.append(trace1)
-
-data = data_full
-layout = go.Layout(
-    margin=dict(
-        l=0,
-        r=0,
-        b=0,
-        t=0
-    )
-)
-fig = go.Figure(data=data, layout=layout)
-py.iplot(fig, filename='PCA with 3 components')
+    fig = go.Figure(data=data, layout=layout)
+    py.iplot(fig, filename='PCA with 3 components')
 
 
 # Another library for interactive plots: https://bokeh.pydata.org/en/latest/docs/gallery/iris_splom.html
 
-# Trying to reconstruct the image
-approximation = pca.inverse_transform(x_train)
 
+if __name__ == "__main__":
+
+    path1 = os.path.join(os.path.dirname(__file__), 'dcms_pix.npy')
+    path2 = os.path.join(os.path.dirname(__file__), 'dcms_id.npy')
+    x_array = np.load(path1)
+    print('Data array loaded.')
+    print(x_array.shape)
+    id_array = np.load(path2)
+    print('ID array loaded.')
+
+    # If available, upload data (issue with gdcm and MacOs).
+    # Otherwise run the generate_image_and_id_arrays(path) function
+    # x_array = np.load('src/util/dcms_pix.npy')
+    # print('Data array loaded.')
+    # id_array = np.load('src/util/dcms_id.npy')
+    # print('ID array loaded.')
+
+
+    # Performing PCA
+    n_components = 2
+    pca = PCA(n_components=n_components)
+    pca = PCA(.95) # It means that scikit-learn choose the minimum number of principal components
+    # such that 95% of the variance is retained.
+
+    x_train = PCA_function(x_array, pca)
+
+    # These things are computationally expensive, so save your models!
+    filename = 'pca_'+str(pca.n_components)+'PC_'+str(time.strftime("%d_%m_%Y"))+'_'+str(time.strftime("%H:%M:%S"))+'.pickle'
+    pickle.dump(pca, open(filename, 'wb'))
+
+    # # Create a dataframe for all the data:
+    # columns = []
+    # for i in range(1, n_components+1):
+    #     string = 'principal component '+ str(i)
+    #     columns.append(string)
+    #
+    #
+    # principalDf = pd.DataFrame(data=x_train, columns=columns)
+    #
+    # static_plotting(principalDf, id_array)
+    # interactive_plotting(principalDf, id_array)
+    #
+    # # Trying to reconstruct the image
+    # # Thanks to: https://github.com/mGalarnyk/Python_Tutorials/blob/master/Sklearn/PCA/PCA_Image_Reconstruction_and_such.ipynb
+    # approximation = pca.inverse_transform(x_train)
+    #
+    # # Plotting
+    # plt.figure(figsize=(8, 4));
+    # # Original Image
+    # plt.subplot(1, 2, 1);
+    # plt.imshow(x_array[0, :, :].reshape(512, 512),
+    #            cmap=plt.cm.gray, interpolation='nearest');
+    # plt.xlabel('?? components', fontsize=14)  # How many components do we really have?
+    # plt.title('Original Image', fontsize=20);
+    # plt.show()
+    #
+    #
+    # # ??? principal components
+    # plt.subplot(1, 2, 2);
+    # plt.imshow(approximation[0].reshape(512, 512),
+    #            cmap=plt.cm.gray, interpolation='nearest');
+    # plt.xlabel('10 components', fontsize=14)
+    # plt.title('50% of Explained Variance', fontsize=20);
+    # plt.show()
+    #
+    # # Trying to get the meaning out the principal components:
+    # plt.plot(np.cumsum(pca.explained_variance_ratio_))
+    # plt.xlabel('number of components')
+    # plt.ylabel('cumulative explained variance')
+    # plt.show()
+    #
+    # # Todo:
+    # # - try out different types of PCA (KernelPCA, SparsePCA, TruncatedSVD, IncrementalPCA)
+    # # - run the computationally more expensive things on Euler
+    #

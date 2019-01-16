7c67a0744b32c251cfcec6b9854480406c9aa5f8
Ines <pereira.inez@gmail.com>
2019-01-16 14:05:15 +0100

    Got plotting of isolated PC to work! Bug to fix now?

--- src/util/dcm_manipulation.py
+++ src/util/dcm_manipulation.py
@@ -1,3 +1,17 @@
+'''
+Interesting resources:
+- https://towardsdatascience.com/pca-using-python-scikit-learn-e653f8989e60
+- Getting the inverse transform: https://github.com/mGalarnyk/Python_Tutorials/blob/master/Sklearn/PCA/PCA_Image_Reconstruction_and_such.ipynb
+-
+
+Thoughts on PCA results:
+- Subjects 8 and 100 are separated from the rest of the lot, along the PC1 axis!
+- So what does PC1 encode??
+    - bear in mind that subjects
+    - Number 8 has a big yaw (the head is quite tilted)
+
+'''
+
 import matplotlib.pyplot as plt
 # from pydicom.data import get_testdata_files
 import os
@@ -20,7 +34,8 @@ import random
 import plotly
 import plotly.plotly as py
 import plotly.graph_objs as go
-plotly.tools.set_credentials_file(username='InesPereira', api_key='R4GwRqWixPIU0ICrXHeO')
+from plotly.graph_objs import *
+plotly.tools.set_credentials_file(username='InesPereira', api_key='wvXCCdZJ04XVr5mDhjn3')
 import pickle
 import time
 
@@ -75,6 +90,11 @@ def PCA_function(x_array, pca):
 # # print(colors)
 
 # Function (courtesy of https://www.quora.com/How-do-I-generate-n-visually-distinct-RGB-colours-in-Python)
+def greyscale_plot(object):
+    plt.figure();
+    plt.imshow(object, cmap=plt.cm.gray, interpolation='nearest');
+    plt.show()
+
 def define_colors(n):
   ret = []
   r = int(random.random() * 256)
@@ -148,7 +168,11 @@ def interactive_plotting(principalDf, id_array):
             l=0,
             r=0,
             b=0,
-            t=0
+            t=0),
+        scene=Scene(
+            xaxis=XAxis(title='Principal Component 1'),
+            yaxis=YAxis(title='Principal Component 2'),
+            zaxis=ZAxis(title='Principal Component 3')
         )
     )
     fig = go.Figure(data=data, layout=layout)
@@ -188,48 +212,132 @@ if __name__ == "__main__":
     filename = 'pca_'+str(pca.n_components)+'PC_'+str(time.strftime("%d_%m_%Y"))+'_'+str(time.strftime("%H:%M:%S"))+'.pickle'
     pickle.dump(pca, open(filename, 'wb'))
 
-    # # Create a dataframe for all the data:
-    # columns = []
-    # for i in range(1, n_components+1):
-    #     string = 'principal component '+ str(i)
-    #     columns.append(string)
-    #
-    #
-    # principalDf = pd.DataFrame(data=x_train, columns=columns)
-    #
-    # static_plotting(principalDf, id_array)
-    # interactive_plotting(principalDf, id_array)
-    #
-    # # Trying to reconstruct the image
-    # # Thanks to: https://github.com/mGalarnyk/Python_Tutorials/blob/master/Sklearn/PCA/PCA_Image_Reconstruction_and_such.ipynb
-    # approximation = pca.inverse_transform(x_train)
-    #
-    # # Plotting
-    # plt.figure(figsize=(8, 4));
-    # # Original Image
-    # plt.subplot(1, 2, 1);
-    # plt.imshow(x_array[0, :, :].reshape(512, 512),
-    #            cmap=plt.cm.gray, interpolation='nearest');
+    # Create a dataframe for all the data:
+    columns = []
+    for i in range(1, n_components+1):
+        string = 'principal component '+ str(i)
+        columns.append(string)
+
+    principalDf = pd.DataFrame(data=x_train, columns=columns)
+
+    static_plotting(principalDf, id_array)
+    interactive_plotting(principalDf, id_array)
+
+    # Trying to reconstruct the image
+    # Thanks to: https://github.com/mGalarnyk/Python_Tutorials/blob/master/Sklearn/PCA/PCA_Image_Reconstruction_and_such.ipynb
+    approximation = pca.inverse_transform(x_train)
+
+    # Plotting
+    plt.figure(figsize=(8, 4));
+    # Original Image
+    plt.subplot(1, 2, 1);
+    plt.imshow(x_array[0, :, :].reshape(512, 512),
+               cmap=plt.cm.gray, interpolation='nearest');
     # plt.xlabel('?? components', fontsize=14)  # How many components do we really have?
-    # plt.title('Original Image', fontsize=20);
-    # plt.show()
-    #
-    #
-    # # ??? principal components
-    # plt.subplot(1, 2, 2);
-    # plt.imshow(approximation[0].reshape(512, 512),
-    #            cmap=plt.cm.gray, interpolation='nearest');
-    # plt.xlabel('10 components', fontsize=14)
-    # plt.title('50% of Explained Variance', fontsize=20);
-    # plt.show()
-    #
-    # # Trying to get the meaning out the principal components:
-    # plt.plot(np.cumsum(pca.explained_variance_ratio_))
-    # plt.xlabel('number of components')
-    # plt.ylabel('cumulative explained variance')
-    # plt.show()
-    #
-    # # Todo:
-    # # - try out different types of PCA (KernelPCA, SparsePCA, TruncatedSVD, IncrementalPCA)
-    # # - run the computationally more expensive things on Euler
-    #
+    plt.title('Original Image', fontsize=20);
+    plt.show()
+
+
+    # ??? principal components
+    plt.subplot(1, 2, 2);
+    plt.imshow(approximation[0].reshape(512, 512),
+               cmap=plt.cm.gray, interpolation='nearest');
+    plt.xlabel('?? components', fontsize=14)
+    plt.title('95% of Explained Variance', fontsize=20);
+    plt.show()
+
+    # Trying to get the meaning out the principal components:
+    plt.plot(np.cumsum(pca.explained_variance_ratio_))
+    plt.xlabel('number of components')
+    plt.ylabel('cumulative explained variance')
+    plt.show()
+
+    # Todo:
+    # - try out different types of PCA (KernelPCA, SparsePCA, TruncatedSVD, IncrementalPCA)
+    # - run the computationally more expensive things on Euler
+
+
+    #######################################
+    # WORKING ON PCA COMPUTED ON LEONHARD #
+    #######################################
+    filename = 'pca_0.95PC_13_01_2019_18:19:42.pickle'
+    pca_model = pickle.load(open('src/util/'+filename, 'rb'))
+    n_components = pca_model.n_components_
+
+    # Perform PCA transformation on data
+    x_reshaped = x_array.reshape(x_array.shape[0], 512*512)
+    x_transformed = pca_model.transform(x_reshaped)
+
+    # Create a dataframe for all the data:
+    columns = []
+    for i in range(1, n_components+1):
+        string = 'principal component '+ str(i)
+        columns.append(string)
+
+    principalDf = pd.DataFrame(data=x_transformed, columns=columns)
+
+    # Plotting the transformed data:
+    static_plotting(principalDf, id_array)
+    interactive_plotting(principalDf, id_array)
+
+    # Creating approximations with all components:
+    approximation = pca_model.inverse_transform(x_transformed)
+    slice_approx = approximation[10,:] # pick a slice to plot
+    approx_reshaped = slice_approx.reshape(512, 512)  # reshape so you get back your image :)
+    greyscale_plot(approx_reshaped)
+
+
+    # Trying to isolate single PC:
+    # https://stackoverflow.com/questions/32750915/pca-inverse-transform-manually
+    PC1 = pca_model.components_[0,:]
+    U_reduced = np.zeros(pca_model.components_.shape)  # Creating a new U matrix which will have only one PC
+    U_reduced[0,:] = PC1  # here we let U_reduced have only PC1
+    PC1_approx = np.dot(x_transformed, U_reduced) + pca_model.mean_  # recreating approximation with only PC1
+    # Save your arrays and spare your computer :)
+    np.save('src/util/PC1_approximation.npy', PC1_approx)
+    greyscale_plot(PC1_approx[10,:].reshape(512, 512))  # resembles an x-ray
+
+    PC2 = pca_model.components_[1,:]
+    U_reduced = np.zeros(pca_model.components_.shape)
+    U_reduced[1,:]=PC2
+    PC2_approx = np.dot(x_transformed, U_reduced) + pca_model.mean_
+    # Save your arrays and spare your computer :)
+    np.save('src/util/PC2_approximation.npy', PC2_approx)
+    greyscale_plot(PC2_approx[12,:].reshape(512, 512))  # maybe encoding pixel values?
+
+    PC3 = pca_model.components_[2,:]
+    U_reduced = np.zeros(pca_model.components_.shape)
+    U_reduced[2,:]=PC3
+    PC3_approx = np.dot(x_transformed, U_reduced) + pca_model.mean_
+    # Save your arrays and spare your computer :)
+    np.save('src/util/PC3_approximation.npy', PC3_approx)
+    greyscale_plot(PC3_approx[12,:].reshape(512, 512))
+
+    # Now let's combine them
+    U_reduced = np.zeros(pca_model.components_.shape)
+    U_reduced[0,:]=PC1
+    U_reduced[1,:]=PC2
+    PC12_approx = np.dot(x_transformed, U_reduced) + pca_model.mean_
+    greyscale_plot(PC12_approx[12,:].reshape(512, 512))
+
+    U_reduced = np.zeros(pca_model.components_.shape)
+    U_reduced[0,:] = PC1
+    U_reduced[1,:] = PC2
+    U_reduced[2,:] = PC3
+    PC123_approx = np.dot(x_transformed, U_reduced) + pca_model.mean_
+    greyscale_plot(PC123_approx[12,:].reshape(512, 512))
+
+    # Removing components:
+    U_reduced = pca_model.components_
+    U_reduced[2,:] = 0
+    PC_less1_approx = np.dot(x_transformed, U_reduced) + pca_model.mean_
+    greyscale_plot(PC_less1_approx[10,:].reshape(512, 512))
+
+
+
+
+
+
+
+
+

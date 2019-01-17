78daf456ee8b4291624b26ffb054b2ca8626b481
Ines <pereira.inez@gmail.com>
2019-01-17 13:59:57 +0100

    Commit before merge with master

--- src/util/get_dcms_for_PCA.py
+++ src/util/get_dcms_for_PCA.py
@@ -1,6 +1,7 @@
 import os
 import csv
 import random
+import re
 
 def list_creator(relative_path):
     with open(relative_path, 'r') as f:
@@ -16,7 +17,11 @@ selected_healthy_corrected = []
 for subject in selected_healthy:
     selected_healthy_corrected.append(subject.replace('-', ''))
 
+
+re_selected_healthy = re.compile(r'\b(?:%s)\b' % '|'.join(selected_healthy_corrected))
+
 os.chdir('/Users/ines/Dropbox/CT_head_trauma')
 
 for file in os.listdir():
-    if file.startswith()
+    m = re_selected_healthy.search(file)
+    if m is not None:
